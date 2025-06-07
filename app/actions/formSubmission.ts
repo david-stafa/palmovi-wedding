2ed8b048e4532678e242e5fcaab24c9f089c68ac
David Stafa <david.stafa@gmail.com>
2025-06-07 20:45:44 +0200

    fix: prisma deploy

--- app/actions/formSubmission.ts
+++ app/actions/formSubmission.ts
@@ -1,6 +1,6 @@
 "use server";
 
-import prisma from "@/lib/prisma";
+import { prisma } from "@/lib/prisma";
 import { FormSchema, formSchema } from "@/lib/types";
 
 export const createConfirmation = async (formData: FormSchema) => {
